55907bc7415ee1ae43e59a377a1759c494456b3c
Gordon Zheng <gordon@greenhouse.io>
2016-04-07 12:08:17 -0400

    Uncomment un-dangerous code

--- nil.rb
+++ nil.rb
@@ -1,13 +1,13 @@
 ObjectSpace.each_object(Class).each do |klass|
-  # klass.send(:define_method, 'nil?') { nil }
-  # klass.send(:define_method, '==') { |_| nil }
-  # klass.send(:define_method, '>') { |_| nil }
-  # klass.send(:define_method, '<') { |_| nil }
-  # klass.send(:define_method, '-') { |_| nil }
+  klass.send(:define_method, 'nil?') { nil }
+  klass.send(:define_method, '==') { |_| nil }
+  klass.send(:define_method, '>') { |_| nil }
+  klass.send(:define_method, '<') { |_| nil }
+  klass.send(:define_method, '-') { |_| nil }
 
   # Dangerous:
-  klass.send(:define_method, 'new') { |*p| nil }
-  klass.send(:define_method, '+') { |_| nil }
+  # klass.send(:define_method, 'new') { |*p| nil }
+  # klass.send(:define_method, '+') { |_| nil }
 end
 
 [true, false].each do |obj|
