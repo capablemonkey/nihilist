a2ef547200b56d3989b58ff4f8046c7efdcf4ac4
Gordon Zheng <gordon@greenhouse.io>
2016-04-07 12:05:53 -0400

    initial commit

---
+++ nihilist.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |gem|
+  gem.authors       =  'nil'
+  gem.email         =  'nil'
+  gem.summary       =  'nil'
+  gem.description   =  'nil'
+
+  gem.files         = 'nil.rb'
+  gem.name          = 'nihilist'
+  gem.version       = '0.0.0'
+end
---
+++ nil.rb
@@ -0,0 +1,15 @@
+ObjectSpace.each_object(Class).each do |klass|
+  # klass.send(:define_method, 'nil?') { nil }
+  # klass.send(:define_method, '==') { |_| nil }
+  # klass.send(:define_method, '>') { |_| nil }
+  # klass.send(:define_method, '<') { |_| nil }
+  # klass.send(:define_method, '-') { |_| nil }
+
+  # Dangerous:
+  klass.send(:define_method, 'new') { |*p| nil }
+  klass.send(:define_method, '+') { |_| nil }
+end
+
+[true, false].each do |obj|
+  obj.class.send(:define_method, :inspect) { "True, or false... does it really matter?" }
+end
